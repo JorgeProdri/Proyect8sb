69eca6552102884ccb0a37ee4d23ac46ea4330a5
JorgeProdri <piguave970@gmail.com>
2023-12-30 21:29:25 -0500

    Updates

--- Proyect8sb/src/components/modalfinca/addfinca.tsx
+++ Proyect8sb/src/components/modalfinca/addfinca.tsx
@@ -1,25 +1,40 @@
+import React, { useState } from "react";
 import "./addfinca.scss";
 import { GridColDef } from "@mui/x-data-grid";
 
-
 type Props = {
-    slug: string;
-    columns: GridColDef[];
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Addfinca = (props: Props) => {
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-const Addfinca = (props:Props) => {
-    
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    
-        props.setOpen(false)
-      };
+    // Verificar si algún campo está vacío
+    const isEmptyField = Object.values(formValues).some((value) => !value.trim());
+
+    if (isEmptyField) {
+      alert("Por favor, completa todos los campos antes de enviar.");
+    } else {
+      // Realizar la lógica de envío del formulario aquí
+      props.setOpen(false);
+    }
+  };
 
   return (
     <div className="adfinca">
-        <div className="modal">
+      <div className="modal">
         <span className="close" onClick={() => props.setOpen(false)}>
           X
         </span>
@@ -27,17 +42,23 @@ const Addfinca = (props:Props) => {
         <form onSubmit={handleSubmit}>
           {props.columns
             .filter((item) => item.field !== "id" && item.field !== "img")
-            .map((column) => (
-              <div className="item">
-                <label>{column.headerName}</label>
-                <input type={column.type} placeholder={column.field} />
+            .map(({ field, type, headerName }) => (
+              <div className="item" key={field}>
+                <label>{headerName}</label>
+                <input
+                  type={type}
+                  placeholder={field}
+                  name={field} // Añade el atributo name para identificar el campo
+                  onChange={handleChange} // Manejar cambios en el valor del campo
+                  required // Campo obligatorio
+                />
               </div>
             ))}
-          <button>Enviar</button>
+          <button type="submit">Enviar</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Addfinca
+export default Addfinca;
