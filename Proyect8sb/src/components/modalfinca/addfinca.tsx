1e4fbcbb8421ab3b90f3b43932169f256000f83b
JorgeProdri <piguave970@gmail.com>
2023-12-30 20:33:28 -0500

    updates

--- Proyect8sb/src/components/modalfinca/addfinca.tsx
+++ Proyect8sb/src/components/modalfinca/addfinca.tsx
@@ -1,4 +1,6 @@
 import "./addfinca.scss";
+import { GridColDef } from "@mui/x-data-grid";
+
 
 type Props = {
     slug: string;
@@ -7,10 +9,35 @@ type Props = {
   };
 
 
-const addfinca = (props:Props) => {
+const Addfinca = (props:Props) => {
+    
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        props.setOpen(false)
+      };
+
   return (
-    <div className="adfinca">addfinca</div>
+    <div className="adfinca">
+        <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Añadir nueva {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          <button>Enviar</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default addfinca
+export default Addfinca
--- Proyect8sb/src/pages/finca/Finca.tsx
+++ Proyect8sb/src/pages/finca/Finca.tsx
@@ -2,46 +2,48 @@ import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./finca.scss";
 import { useState } from "react";
-import Add from "../../components/add/Add";
+import AddFinca from "../../components/modalfinca/addfinca"; 
 import { userRows } from "../../data";
 
 const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "codigo",
-      type: "int",
-      headerName: "Code",
-      width: 150,
-    },
-    {
-      field: "nombre",
-      type: "string",
-      headerName: "Name",
-      width: 150,
-    },
-    {
-      field: "direccion",
-      type: "string",
-      headerName: "Direccion",
-      width: 200,
-    },
-    {
-      field: "contacto",
-      type: "string",
-      headerName: "Contacto",
-      width: 200,
-    },
-  ];
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "codigo",
+    type: "int",
+    headerName: "Code",
+    width: 150,
+  },
+  {
+    field: "nombre",
+    type: "string",
+    headerName: "Name",
+    width: 150,
+  },
+  {
+    field: "direccion",
+    type: "string",
+    headerName: "Direccion",
+    width: 200,
+  },
+  {
+    field: "contacto",
+    type: "string",
+    headerName: "Contacto",
+    width: 200,
+  },
+];
+
 const Finca = () => {
   const [open, setOpen] = useState(false);
+
   return (
     <div className="users">
       <div className="info">
-        <h1>Finca</h1>
-        <button onClick={() => setOpen(true)}>Añadir Finca</button>
+        <h1>Hacienda</h1>
+        <button onClick={() => setOpen(true)}>Añadir Hacienda</button>
       </div>
-      <DataTable slug="finca" columns={columns} rows={userRows} />
-      {open && <Add slug="finca" columns={columns} setOpen={setOpen} />}
+      <DataTable slug="Hacienda" columns={columns} rows={userRows} />
+      {open && <AddFinca slug="Hacienda" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
