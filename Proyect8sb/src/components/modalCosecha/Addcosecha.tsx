d02009c554230bb7ea4fbcfe3e8ed44952df98c6
JorgeProdri <piguave970@gmail.com>
2024-01-07 15:53:08 -0500

    UPDATE

---
+++ Proyect8sb/src/components/modalCosecha/Addcosecha.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./addcosecha.scss"; // Asegúrate de tener este archivo de estilos
+import { GridColDef } from "@mui/x-data-grid";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const AddCosecha = (props: Props) => {
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const isEmptyField = Object.values(formValues).some((value) => !value.trim());
+
+    if (isEmptyField) {
+      alert("Por favor, completa todos los campos antes de enviar.");
+    } else {
+      props.setOpen(false);
+    }
+  };
+
+  return (
+    <div className="addcosecha">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Añadir nueva {props.slug}</h1>
+        <div className="logo">
+          <img src="logoC.svg" alt="" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map(({ field, type, headerName }) => (
+              <div className={`item ${field === "codigo" ? "hidden" : ""}`} key={field}>
+                <label>{headerName}</label>
+                <input
+                  type={type}
+                  placeholder={field}
+                  name={field}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            ))}
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddCosecha;
---
+++ Proyect8sb/src/pages/cosecha/Cosecha.tsx
@@ -0,0 +1,52 @@
+// Cosecha.jsx
+
+import { GridColDef } from "@mui/x-data-grid";
+import DataTableH from "../../components/datatableH/DataTableH";
+import "./cosecha.scss"; // Asegúrate de tener el archivo de estilos adecuado
+import { useState } from "react";
+import AddCosecha from "../../components/modalCosecha/Addcosecha";
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "codigo",
+    type: "int",
+    width: 150,
+  },
+  {
+    field: "nombre de la cosecha",
+    type: "string",
+    headerName: "Nombre",
+    width: 150,
+  },
+  {
+    field: "fecha_inicio",
+    type: "date",
+    headerName: "Fecha de Inicio",
+    width: 200,
+  },
+  {
+    field: "fecha_fin",
+    type: "date",
+    headerName: "Fecha de Fin",
+    width: 200,
+  },
+];
+
+const Cosecha = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="cosecha">
+      <div className="info">
+        <h1>Cosecha</h1>
+        <button onClick={() => setOpen(true)}>Añadir Cosecha</button>
+      </div>
+      <DataTableH slug="Cosecha" columns={columns} rows={userRows} />
+      {open && <AddCosecha slug="Cosecha" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
+};
+
+export default Cosecha;
