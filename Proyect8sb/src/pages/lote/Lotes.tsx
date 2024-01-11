c08d0590c01c25d163b04aba90f7831532093be4
JorgeProdri <piguave970@gmail.com>
2024-01-10 19:00:03 -0500

    Update

--- Proyect8sb/src/pages/lote/Lotes.tsx
+++ Proyect8sb/src/pages/lote/Lotes.tsx
@@ -1,40 +1,75 @@
 import { GridColDef } from "@mui/x-data-grid";
 import DataTableH from "../../components/datatableH/DataTableH";
-import "./lotes.scss"; 
-import { useState } from "react";
-import AddLotes from "../../components/modalLotes/AddLotes"; 
-import { userRows } from "../../data";
+import "./lotes.scss";
+import { useState, useEffect } from "react";
+import AddLotes from "../../components/modalLotes/AddLotes";
+import axios from "axios";
+
+interface Lote {
+  id: number;
+  cod_lote: number;
+  nomb_lote: string;
+  dimenx_lote: number;
+  dimeny_lote: number;
+  estado_lote: string; // Asumiendo que estado_lote es de tipo string, ajusta según tus datos reales
+  cod_hacienda: number;
+}
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
   {
-    field: "codigo",
-    type: "int",
+    field: "cod_lote",
+    type: "number",
     width: 100,
   },
   {
-    field: "nombre del lote",
+    field: "nomb_lote",
     type: "string",
     headerName: "Nombre",
     width: 120,
   },
   {
-    field: "Dimension x",
+    field: "dimenx_lote",
     type: "number",
     headerName: "Dimension x",
     width: 150,
   },
   {
-    field: "Dimension y",
+    field: "dimeny_lote",
     type: "number",
     headerName: "Dimension y",
     width: 150,
   },
- 
+  {
+    field: "estado_lote",
+    type: "string",
+    headerName: "Estado",
+    width: 100,
+  },
+  {
+    field: "cod_hacienda",
+    type: "number",
+    headerName: "Código Hacienda",
+    width: 150,
+  },
 ];
 
 const Lotes = () => {
   const [open, setOpen] = useState(false);
+  const [lotesData, setLotesData] = useState<Lote[]>([]);
+
+  useEffect(() => {
+    axios.get("https://simulacion7sb.000webhostapp.com/8SB/lote.php")
+      .then((response) => {
+        const lotesConId: Lote[] = response.data.map((lote: Lote) => ({
+          ...lote,
+          id: lote.cod_lote,
+        }));
+        setLotesData(lotesConId);
+      })
+      .catch((error) => {
+        console.error("Error al obtener datos:", error);
+      });
+  }, []);
 
   return (
     <div className="lotes">
@@ -42,7 +77,7 @@ const Lotes = () => {
         <h1>Lotes</h1>
         <button onClick={() => setOpen(true)}>Añadir Lote</button>
       </div>
-      <DataTableH slug="Lotes" columns={columns} rows={userRows} />
+      <DataTableH slug="Lotes" columns={columns} rows={lotesData} />
       {open && <AddLotes slug="Lote" columns={columns} setOpen={setOpen} />}
     </div>
   );
